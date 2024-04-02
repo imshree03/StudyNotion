94866c1bf6b10716a67fff0f3f4503b68f345cc8
imshree03 <shreebhosale24hrs@gmail.com>
2024-04-02 23:15:02 +0530

    Course model

---
+++ StudyNotion/Server/models/Course.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  courseName: {
+    type: String,
+  },
+  courseDescription: {
+    type: String,
+  },
+  instructor: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  whatWillYouLearn: {
+    type: String,
+  },
+  courseContent: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Section",
+    },
+  ],
+  ratingAndReviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "RatingAndReview",
+    },
+  ],
+  price: {
+    type: Number,
+  },
+  thumbnail: {
+    type: String,
+  },
+  tag: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Tag",
+  },
+  studentsEnrolled: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+});
+
+module.exports = mongoose.model("Course", courseSchema);
