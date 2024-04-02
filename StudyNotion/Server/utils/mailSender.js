1365b7d688d96afe966697689c31e84a84865ad8
imshree03 <shreebhosale24hrs@gmail.com>
2024-04-02 23:17:04 +0530

    send mail utility function

---
+++ StudyNotion/Server/utils/mailSender.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const mailSender = async (email, title, body) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+      secure: false,
+    });
+
+    let info = transporter.sendMail({
+      from: "StudyNotion | Codehelp",
+      to: `${email}`,
+      subject: `${title}`,
+      html: `${body}`,
+    });
+    console.log(info.response);
+    return info;
+  } catch (error) {
+    console.log(error.message);
+    return error.message;
+  }
+};
+
+module.exports = mailSender;
