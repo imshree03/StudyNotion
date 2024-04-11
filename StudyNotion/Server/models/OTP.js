bb109db1d5ff3438823d362eab51fb7f23b2ad65
imshree03 <shreebhosale24hrs@gmail.com>
2024-04-11 13:37:21 +0530

    Email Verification added

--- StudyNotion/Server/models/OTP.js
+++ StudyNotion/Server/models/OTP.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const mailSender = require("../utils/mailSender");
+const emailTemplate = require("../mail/templates/emailVerifictionTemplate");
 
 const OTPSchema = new mongoose.Schema({
   email: {
@@ -21,8 +22,8 @@ async function sendVerificationEmail(email, otp) {
   try {
     const mailResponse = await mailSender(
       email,
-      "Verification Email from StudyNotion",
-      otp
+      "Verification Email",
+      emailTemplate(otp)
     );
     console.log("Email sent Successfully", mailResponse);
   } catch (error) {
