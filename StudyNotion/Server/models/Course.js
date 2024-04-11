2fd1dc4b4f580271c9afee74a36d0ab4c7eba8f2
imshree03 <shreebhosale24hrs@gmail.com>
2024-04-11 11:49:06 +0530

    Course Model Updated

--- StudyNotion/Server/models/Course.js
+++ StudyNotion/Server/models/Course.js
@@ -34,18 +34,26 @@ const courseSchema = new mongoose.Schema({
     type: String,
   },
   tag: {
+    type: [String],
+    required: true,
+  },
+  category: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Tag",
+    ref: "Category",
   },
   studentsEnrolled: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
   ],
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
+  instructions: {
+    type: [String],
+  },
+  status: {
+    type: String,
+    enum: ["Draft", "Published"],
   },
 });
 
