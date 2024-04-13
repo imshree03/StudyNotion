fac489f02b67d3e04bc3df4759d559d07d5154d3
imshree03 <shreebhosale24hrs@gmail.com>
2024-04-13 21:20:34 +0530

    SubSection Controllers

---
+++ StudyNotion/Server/controllers/SubSection.js
@@ -0,0 +1,134 @@
+const SubSection = require("../models/SubSection");
+const Section = require("../models/Section");
+const { uploadImageToCloudinary } = require("../utils/imageUploader");
+
+exports.createSubsection = async (req, res) => {
+  try {
+    const { sectionId, title, timeDuration, description } = req.body;
+    const video = req.files.videoFile;
+
+    if (!sectionId || !title || !timeDuration || !description || !video) {
+      return res.status(404).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const uploadDetails = await uploadImageToCloudinary(
+      video,
+      process.env.FOLDER_NAME
+    );
+
+    const SubSectionDetails = await SubSection.create({
+      title,
+      timeDuration: `${uploadDetails.duration}`,
+      description,
+      videoUrl: uploadDetails.secure_url,
+    });
+
+    const updatedSection = await Section.findByIdAndUpdate(
+      { _id: sectionId },
+      {
+        $push: {
+          subSection: SubSectionDetails._id,
+        },
+      },
+      {
+        new: true,
+      }
+    ).populate("subSection");
+
+    return res.status(200).json({
+      success: true,
+      message: "Sub Section Created Successfully",
+      updatedSection,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+exports.updateSubSection = async (req, res) => {
+  try {
+    const { sectionId, title, description } = req.body;
+    const subSection = await SubSection.findById(sectionId);
+
+    if (!subSection) {
+      return res.status(404).json({
+        success: false,
+        message: "SubSection not found",
+      });
+    }
+
+    if (title !== undefined) {
+      subSection.title = title;
+    }
+
+    if (description !== undefined) {
+      subSection.description = description;
+    }
+
+    if (req.files && req.files.video !== undefined) {
+      const video = req.files.video;
+      const uploadDetails = await uploadImageToCloudinary(
+        video,
+        process.env.FOLDER_NAME
+      );
+      subSection.videoUrl = uploadDetails.secure_url;
+      subSection.timeDuration = `${uploadDetails.secure_url}`;
+    }
+
+    await subSection.save();
+
+    return res.json({
+      success: true,
+      message: "Section updated successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "An error occured while updating the section",
+    });
+  }
+};
+
+exports.deleteSubSection = async (req, res) => {
+  try {
+    const { subSectionId, sectionId } = req.body;
+    await Section.findByIdAndUpdate(
+      { _id: sectionId },
+      {
+        $pull: {
+          subSection: subSectionId,
+        },
+      }
+    );
+
+    const subSection = await SubSection.findByIdAndDelete({
+      _id: subSectionId,
+    });
+
+    if (!subSection) {
+      return res.status(404).json({
+        success: false,
+        message: "SubSection not found",
+      });
+    }
+
+    return res.json({
+      success: true,
+      message: "SubSection Deleted Successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred while deleting the SubSection",
+    });
+  }
+};
