4cb48253395a78cd549c0dfd8a3bcb8ef8915bd2
imshree03 <shreebhosale24hrs@gmail.com>
2024-04-14 22:22:00 +0530

    index file (all apis)

---
+++ StudyNotion/Server/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const db = require("./config/database");
+
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const courseRoutes = require("./routes/Course");
+const paymentRoutes = require("./routes/Payment");
+
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+db.connect();
+
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
+
+cloudinaryConnect();
+
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: "Your Server is up and Running...",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up and running ${port}`);
+});
