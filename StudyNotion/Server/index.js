94e491757641fbcb38570c428e42b48b94ae52e3
imshree03 <shreebhosale24hrs@gmail.com>
2024-07-14 00:25:31 +0530

    u

--- StudyNotion/Server/index.js
+++ StudyNotion/Server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://study-notion-shreebhosale.vercel.app/",
+    origin: "https://study-notion-shreebhosale.vercel.app",
     credentials: true,
   })
 );
